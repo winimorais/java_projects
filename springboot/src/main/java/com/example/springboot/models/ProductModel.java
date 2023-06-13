74396e0cebdffd077fef29e14f72395f3973ab30
Winicius <winimorais@gmail.com>
2023-06-13 10:57:41 -0300

    adding database integration

---
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,22 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal price;
+}
